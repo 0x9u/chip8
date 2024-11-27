150d1740d9d415a95ce17feb6028ff39597177dd
oliver po <oliver.po2882@gmail.com>
2024-11-27 11:12:24 +1000

    prepare state for chip8 and cmake and other initial files

---
+++ src/state.cpp
@@ -0,0 +1,38 @@
+#include "state.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <vector>
+
+#include "font.hpp"
+
+void State::LoadROM(const char *filename) {
+    std::ifstream file;
+    try {
+        file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        file.open(filename, std::ios::binary | std::ios::ate);
+    } catch (const std::ios_base::failure &e) {
+        std::cerr << filename << ":" << e.what() << "\n";
+        exit(EXIT_FAILURE);
+    }
+
+    // gets file size to create buffer
+    std::streampos size = file.tellg();
+    std::vector<char> buffer(size);
+    file.read(buffer.data(), size);
+    file.close();
+
+    uint8_t *dest = memory + START_ADDRESS;
+    std::memcpy(dest, buffer.data(), size);
+}
+
+State::State() {
+    pc = START_ADDRESS;
+
+    uint8_t *dest = memory + FONTSET_START_ADDRESS;
+    std::memcpy(dest, fontset, FONTSET_SIZE);
+
+    rand_gen.seed(std::chrono::system_clock::now().time_since_epoch().count());
+    rand_byte = std::uniform_int_distribution<uint8_t>(0, 255U);
+}
